d9de9014a0a413342e9c67dc6a25a8ed93260054
carlleton <carlleton@hotmail.com>
2018-02-26 17:52:52 +0800

    加个id校验

--- server/api/posts.js
+++ server/api/posts.js
@@ -1,11 +1,20 @@
 import Posts from '../models/Posts'
 const router = require('koa-router')()
 const db = require('../../util/db')
+const _ = require('lodash')
 
 let postsModel = new Posts()
 
 router.get('/:id', async (ctx, next) => {
   var id = ctx.params.id
+  if (!_.isInteger(id)) {
+    ctx.status = 404
+    ctx.body = {
+      code: 404,
+      message: 'no result'
+    }
+    return
+  }
   var result = await postsModel.findOne(id)
   ctx.status = 200
   ctx.body = result.result
@@ -49,7 +58,7 @@ router.post('/add', async (ctx, next) => {
 router.post('/update', async (ctx, next) => {
   var body = ctx.request.body
   var params = {
-    id:body.id,
+    id: body.id,
     title: body.title,
     content: body.content,
     cid: body.cid,
